3ecadfe35b188b217035794fe7a1189bbba8d531
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2023-04-07 10:14:08 +0900

    [level 1] Title: 시저 암호, Time: 2.58 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,32 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("a b Z", 4));
+    }
+}
+
+class Solution {
+    public String solution(String s, int n) {
+        StringBuffer sb = new StringBuffer();
+        char[] x = s.toCharArray();
+        for (char c : x) {
+            if (Character.isUpperCase(c)) {
+                int move = c + n;
+                if (move > 'Z') {
+                    move = 'A' + move % ('Z' + 1);
+                }
+                c = (char) move;
+            }
+            if (Character.isLowerCase(c)) {
+                int move = c + n;
+                if (move > 'z') {
+                    move = 'a' + move % ('z' + 1);
+                }
+                c = (char) move;
+            }
+
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
