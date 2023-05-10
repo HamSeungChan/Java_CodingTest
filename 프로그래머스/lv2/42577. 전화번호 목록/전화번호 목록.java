7697e88ba36e4d9fb3d8d3e568eb9474e40ba376
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2023-05-10 21:09:09 +0900

    [level 2] Title: 전화번호 목록, Time: 224.07 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String phone : phone_book){
+            map.put(phone,phone.length());
+        }
+        
+        for(int i=0; i<phone_book.length;i++){
+            for(int j=1; j<phone_book[i].length();j++){
+                if(map.containsKey(phone_book[i].substring(0,j))){
+                    return false;
+                }
+            }
+        }
+        return answer;
+    }
+}
