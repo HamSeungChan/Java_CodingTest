ad22b44e7e3c5233a8a799cf60ce56883e90f6a1
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2023-05-20 13:30:40 +0900

    [Gold V] Title: 치킨 배달, Time: 456 ms, Memory: 25164 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    static int answer = Integer.MAX_VALUE;
+    static List<Point> chickenList = new ArrayList<>();
+    static List<Point> houseList = new ArrayList<>();
+    static int m;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        m = sc.nextInt();
+
+        int[][] map = new int[n][n];
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int tmp = sc.nextInt();
+                map[i][j] = tmp;
+
+                if (tmp == 2) {
+                    chickenList.add(new Point(i, j));
+                }
+
+                if (tmp == 1) {
+                    houseList.add(new Point(i, j));
+                }
+            }
+        }
+
+        selectChicken(0, 0, new int[m]);
+        System.out.println(answer);
+    }
+
+    static void selectChicken(int start, int value, int[] select) {
+        if (value == select.length) {
+            int totalDistance = 0;
+            for (Point house : houseList) {
+                int minDistance = Integer.MAX_VALUE;
+                for (int x : select) {
+                    minDistance = Math.min(minDistance, Math.abs(house.x - chickenList.get(x).x) + Math.abs(house.y - chickenList.get(x).y));
+                }
+                totalDistance += minDistance;
+            }
+            answer = Math.min(totalDistance,answer);
+        } else {
+            for (int i = start; i < chickenList.size(); i++) {
+                select[value] = i;
+                selectChicken(i + 1, value + 1, select);
+            }
+        }
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
