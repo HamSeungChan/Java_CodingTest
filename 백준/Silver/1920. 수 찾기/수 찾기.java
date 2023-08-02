f19b64e4521703ee139aeb6cc556262fa85f06d6
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2023-08-02 11:00:40 +0900

    [Silver IV] Title: 수 찾기, Time: 1452 ms, Memory: 54080 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,40 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        long[] array = new long[n];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token;
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+        token = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            array[i] = sc.nextLong();
+            array[i] = Integer.parseInt(token.nextToken());
         }
         Arrays.sort(array);
-
-        int m = sc.nextInt();
-        long[] findArray = new long[m];
-        for (int i = 0; i < m; i++) {
-            findArray[i] = sc.nextLong();
+        int find = Integer.parseInt(br.readLine());
+        int[] answer = new int[find];
+        token = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < find; i++) {
+            answer[i] = Integer.parseInt(token.nextToken());
         }
 
-        for (long find : findArray) {
+        for (int x : answer) {
             int lt = 0;
             int rt = array.length - 1;
-            int answer = 0;
-
+            int print = 0;
             while (lt <= rt) {
                 int middle = (lt + rt) / 2;
-                if (array[middle] == find) {
-                    answer = 1;
+                if (array[middle] == x) {
+                    print = 1;
                     break;
-                } else if (array[middle] > find) {
+                } else if (array[middle] > x) {
                     rt = middle - 1;
                 } else {
                     lt = middle + 1;
                 }
             }
-            System.out.println(answer);
+            System.out.println(print);
         }
+
+
     }
 }
