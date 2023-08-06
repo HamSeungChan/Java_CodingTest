42ba0dd6f67f44bbd9a1646f6b5eb86749aa3d8a
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2023-08-06 16:51:15 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,15 +3,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static final int PRIME = 1234567891;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         char[] c = br.readLine().toCharArray();
 
         int sum = 0;
+        int pow = 1;
         for (int i = 0; i < n; i++) {
-            sum += (c[i] - 'a' + 1) * (int) Math.pow(31, i);
+            sum += (c[i] - 'a' + 1) * pow % PRIME;
+            pow = (31 * pow) % PRIME;
         }
-        System.out.println(sum);
+        System.out.println(sum % PRIME);
     }
 }
