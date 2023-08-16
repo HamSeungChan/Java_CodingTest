e7565b64d5863f59528198ba6921a067315ac688
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2023-08-16 17:22:42 +0900

    [Gold IV] Title: N-Queen, Time: 4392 ms, Memory: 12512 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -6,23 +6,25 @@ public class Main {
 
 	static int n;
 	static int[] array;
+	static boolean[] leftCross;
+	static boolean[] rightCross;
 	static int answer;
+	static int size;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		answer = 0;
 		array = new int[n + 1];
-		
-		setQuuen(1);
+		answer = 0;
+		leftCross = new boolean[2 * n - 1];
+		rightCross = new boolean[2 * n - 1];
+		size = n - 1;
+
+		setQueen(1);
 		System.out.println(answer);
 	}
 
-	public static void setQuuen(int row) {
-
-		if (!isAvailable(row - 1)) {
-			return;
-		}
+	public static void setQueen(int row) {
 
 		if (row > n) {
 			answer++;
@@ -30,20 +32,27 @@ public class Main {
 		}
 
 		for (int c = 1; c <= n; c++) {
-			array[row] = c;
-			setQuuen(row + 1);
-		}
 
+			if (isAvailable(row, c)) {
+				leftCross[row - c + size] = true;
+				rightCross[row + c - 2] = true;
+				
+				array[row] = c;
+				setQueen(row + 1);
+				
+				rightCross[row + c - 2] = false;
+				leftCross[row - c + size] = false;
+			}
+		}
 	}
 
-	public static boolean isAvailable(int row) {
+	public static boolean isAvailable(int row, int c) {
 
 		for (int i = 1; i < row; i++) {
-			if (array[i] == array[row] || row - i == Math.abs(array[i] - array[row])) {
+			if (array[i] == c || leftCross[row - c + size] == true || rightCross[row + c - 2] == true) {
 				return false;
 			}
 		}
-
 		return true;
 	}
 
