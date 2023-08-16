b31c25684e0dc20afe88ba6b47a891be5e05bc78
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2023-08-16 15:24:43 +0900

    [Gold II] Title: 빵집, Time: 1724 ms, Memory: 502756 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static String[][] array;
+	static boolean[][] pipeLine;
+	static int answer, r, c;
+	static boolean flag;
+	static final int[] MOVE_X = { -1, 0, 1 };
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+		r = Integer.parseInt(token.nextToken());
+		c = Integer.parseInt(token.nextToken());
+
+		pipeLine = new boolean[r][c];
+		array = new String[r][c];
+		answer = 0;
+
+		for (int i = 0; i < r; i++) {
+			String[] tmp = br.readLine().split("");
+			for (int j = 0; j < c; j++) {
+				array[i][j] = tmp[j];
+			}
+		}
+
+		for (int i = 0; i < r; i++) {
+			flag = false;
+			setPipe(i, 0);
+		}
+
+		System.out.println(answer);
+
+	}
+
+	public static void setPipe(int x, int y) {
+
+		
+		if (y == c - 1) {
+			answer++;
+			flag = true;
+			return;
+		}
+
+		for (int i = 0; i < MOVE_X.length; i++) {
+			int moveX = x + MOVE_X[i];
+			int moveY = y + 1;
+			if (checkValid(moveX, moveY)) {
+				pipeLine[moveX][moveY] = true;
+				setPipe(moveX, moveY);
+				if(flag) {
+					break;
+				}
+			} 
+		}
+	}
+
+	public static boolean checkValid(int x, int y) {
+		if (x >= 0 && x < r && !array[x][y].equals("x") && pipeLine[x][y] == false) {
+			return true;
+		}
+		return false;
+	}
+
+}
