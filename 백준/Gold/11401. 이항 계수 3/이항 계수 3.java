8ef4ee29451ca7f3ae3e9d3b5f57484a1b6012e9
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2023-10-11 14:50:05 +0900

    [Gold I] Title: 이항 계수 3, Time: 272 ms, Memory: 159596 KB -BaekjoonHub

---
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static final long DIV = 1_000_000_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(token.nextToken());
+        int k = Integer.parseInt(token.nextToken());
+
+        long top = factorial(n);
+        long bottom = (factorial(k) * factorial(n - k)) % DIV;
+        System.out.println((top * pow(bottom, DIV - 2) % DIV) % DIV);
+
+    }
+
+    public static long factorial(long n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return (n * factorial(n - 1)) % DIV;
+    }
+
+    public static long pow(long base, long expo) {
+        long result = 1;
+
+        while (expo > 0) {
+            if (expo % 2 == 1) {
+                result *= base;
+                result %= DIV;
+                expo--;
+            }
+            base = (base * base) % DIV;
+            expo /= 2;
+        }
+        return result;
+    }
+}
