c2293c8833c3592f8c3bece6bb1f02ecd4f8a353
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-01-16 08:30:44 +0900

    [Silver III] Title: 주유소, Time: 296 ms, Memory: 31352 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] distance = new int[n - 1];
+
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < distance.length; i++) {
+            distance[i] = Integer.parseInt(token.nextToken());
+        }
+
+        int[] array = new int[n];
+        token = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.parseInt(token.nextToken());
+        }
+
+        int answer = array[0] * distance[0];
+        int min = array[0];
+
+        for (int i = 1; i < distance.length; i++) {
+            min = Math.min(min, array[i]);
+            answer += min * distance[i];
+        }
+
+        System.out.println(answer);
+
+    }
+}
