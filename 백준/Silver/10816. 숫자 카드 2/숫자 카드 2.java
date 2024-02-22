5242d8b64f395eb7676ddfea30dbdaaebd961f73
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-02-22 12:20:17 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1240 ms, Memory: 215464 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,34 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-    static StringBuilder sb = new StringBuilder();
+    static int[] array;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<Integer, Integer> map = new HashMap<>();
         int n = Integer.parseInt(br.readLine());
+
+
         StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+        Map<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < n; i++) {
             int tmp = Integer.parseInt(token.nextToken());
-            map.put(tmp, map.getOrDefault(tmp, 0)+1);
+            map.put(tmp, map.getOrDefault(tmp, 0) + 1);
         }
+
+        array = new int[map.size()];
+        int index = 0;
+        for (int i : map.keySet()) {
+            array[index] = i;
+            index++;
+        }
+        Arrays.sort(array);
+
         int m = Integer.parseInt(br.readLine());
+        int[] answer = new int[m];
         token = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < m; i++) {
-            int tmp = Integer.parseInt(token.nextToken());
-            if(!map.containsKey(tmp)){
-                sb.append(0).append(" ");
-            }
-            else{
-                sb.append(map.get(tmp)).append(" ");
-            }
+            answer[i] = map.getOrDefault(Integer.parseInt(token.nextToken()), 0);
+        }
 
+        StringBuilder sb = new StringBuilder();
+        for (int i : answer) {
+            sb.append(i).append(" ");
         }
         System.out.println(sb);
     }
