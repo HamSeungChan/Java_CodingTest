6074c0b62a11dfc055294dda02f571226be6fde9
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-02-25 13:20:24 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 464 ms, Memory: 125428 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,40 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] array = new int[n];
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(token.nextToken());
+        }
+        int[] dp = new int[n];
+        dp[0] = array[0];
+        int lastIndex = 1;
 
-        for(int i = 0 ; i < n; i++) {
-            int value = arr[i] = Integer.parseInt(st.nextToken());
-            if(value > list.get(list.size() - 1)) list.add(value);
-            else{
-                int left = 0;
-                int right = list.size() - 1;
-
-                while(left < right){
-                    int mid = (left + right) >> 1;
-                    if(list.get(mid) >= value){
-                        right = mid;
-                    }else{
-                        left = mid + 1;
-                    }
-                }
-                list.set(right, value);
+        for (int i = 1; i < n; i++) {
+            if (dp[lastIndex - 1] < array[i]) {
+                dp[lastIndex] = array[i];
+                lastIndex++;
+            } else {
+                int index = lowerBound(dp, array[i], lastIndex);
+                dp[index] = array[i];
             }
         }
-        System.out.println(list.size() - 1);
+        System.out.println(lastIndex);
     }
 
+    public static int lowerBound(int[] dp, int value, int lastIndex) {
+        int lt = 0;
+        int rt = lastIndex;
+
+        while (lt < rt) {
+            int mid = lt + (rt - lt) / 2;
+            if (dp[mid] >= value) {
+                rt = mid;
+            } else {
+                lt = mid + 1;
+            }
+        }
+        return lt;
+    }
 }
