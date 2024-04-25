e4b810a5587623a1b36a9579ce0e7f28c8da2d93
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-04-25 21:11:37 +0900

    [Silver II] Title: 최소 힙, Time: 288 ms, Memory: 24616 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -4,23 +4,25 @@ import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int n = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> q = new PriorityQueue<>();
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		StringBuilder sb = new StringBuilder();
-		for(int i =0; i< n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			int tmp = Integer.parseInt(br.readLine());
-			if(tmp != 0) {
-				q.offer(tmp);
+			if (tmp == 0) {
+				if (pq.isEmpty()) {
+					sb.append(0).append("\n");
+					continue;
+				}
+				sb.append(pq.poll()).append("\n");
 				continue;
 			}
-			
-			if(q.isEmpty()) {
-				sb.append(0).append("\n");
-			}else {
-				sb.append(q.poll()).append("\n");
-			}
+			pq.offer(tmp);
 		}
 		System.out.println(sb);
 	}
