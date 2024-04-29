645be872c981dcc2e7d311a249221e8b03764726
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-04-29 23:01:07 +0900

    [Silver V] Title: 색종이, Time: 80 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,33 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[][] array = new int[101][101];
+
 		int n = Integer.parseInt(br.readLine());
-		for (int p = 0; p < n; p++) {
-			String[] s = br.readLine().split(" ");
-			int x = Integer.parseInt(s[0]);
-			int y = Integer.parseInt(s[1]);
+		boolean[][] array = new boolean[101][101];
+
+		StringTokenizer token;
+		for (int t = 0; t < n; t++) {
+
+			token = new StringTokenizer(br.readLine(), " ");
+
+			int x = Integer.parseInt(token.nextToken());
+			int y = Integer.parseInt(token.nextToken());
 
 			for (int i = x; i < x + 10; i++) {
 				for (int j = y; j < y + 10; j++) {
-					array[i][j] = 1;
+					array[i][j] = true;
 				}
 			}
+
 		}
 
 		int count = 0;
-		for (int i = 1; i <= 100; i++) {
-			for (int j = 1; j <= 100; j++) {
-				if (array[i][j] == 1) {
+		for (int i = 0; i < 101; i++) {
+			for (int j = 0; j < 101; j++) {
+				if (array[i][j])
 					count++;
-				}
 
 			}
 		}
+
 		System.out.println(count);
 	}
 }
