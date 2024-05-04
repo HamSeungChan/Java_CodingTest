70980355781c1726cfa9ba48f4ccbc2de56b141f
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-05-04 13:13:44 +0900

    [Silver III] Title: N과 M (8), Time: 112 ms, Memory: 16328 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -6,37 +6,42 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int n;
-	static int m;
+	static int n, m;
+	static int[] array;
 	static StringBuilder sb = new StringBuilder();
-	static int[] number;
 
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+
 		n = Integer.parseInt(token.nextToken());
 		m = Integer.parseInt(token.nextToken());
 
+		array = new int[n];
+
 		token = new StringTokenizer(br.readLine(), " ");
-		number = new int[n];
 		for (int i = 0; i < n; i++) {
-			number[i] = Integer.parseInt(token.nextToken());
+			array[i] = Integer.parseInt(token.nextToken());
 		}
-		Arrays.sort(number);
-		permutation(new int [m], 0, 0);
-		System.out.println(sb);
+
+		Arrays.sort(array);
+		comb(0, 0, new int[m]);
+		System.out.print(sb);
 	}
 
-	public static void permutation(int[] array, int count, int start) {
-		if (count == m) {
-			for (int x : array) {
-				sb.append(x).append(" ");
+	public static void comb(int index, int start, int[] pick) {
+		if (index == m) {
+
+			for (int i : pick) {
+				sb.append(array[i]).append(" ");
 			}
 			sb.append("\n");
+
 		} else {
-			for (int i = start; i < number.length; i++) {
-				array[count] = number[i];
-				permutation(array, count + 1, i);
+			for (int i = start; i < n; i++) {
+				pick[index] = i;
+				comb(index + 1, i, pick);
 			}
 		}
 	}
