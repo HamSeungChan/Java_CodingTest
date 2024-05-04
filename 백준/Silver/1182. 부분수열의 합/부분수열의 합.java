d3a7b3f093e8c8241a774bc9ca5d68e6a2398980
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-05-04 14:23:40 +0900

    [Silver II] Title: 부분수열의 합, Time: 168 ms, Memory: 12476 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -5,33 +5,54 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n;
-    static int s;
-    static int[] array;
-    static int answer = 0;
+	static int n, s, answer = 0;
+	static int[] array;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(token.nextToken());
-        s = Integer.parseInt(token.nextToken());
-        array = new int[n];
-        token = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(token.nextToken());
-        }
-        powerSet(0, 0, 0);
-        System.out.println(answer);
-    }
+	public static void main(String[] args) throws IOException {
 
-    public static void powerSet(int index, int zeroCount, int sum) {
-        if (index == n) {
-            if(zeroCount != n && sum == s){
-                answer++;
-            }
-        } else {
-            powerSet(index + 1, zeroCount, sum + array[index]);
-            powerSet(index + 1, zeroCount + 1, sum);
-        }
-    }
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+
+		n = Integer.parseInt(token.nextToken());
+		s = Integer.parseInt(token.nextToken());
+
+		array = new int[n];
+		token = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < n; i++) {
+			array[i] = Integer.parseInt(token.nextToken());
+		}
+
+		subset(new boolean[n], 0, 0);
+		System.out.println(answer);
+	}
+
+	public static void subset(boolean[] pick, int count, int index) {
+		if (index == n) {
+
+			if (count == 0) {
+				return;
+			}
+
+			int sum = 0;
+			for (int i = 0; i < pick.length; i++) {
+				if (pick[i])
+					sum += array[i];
+			}
+
+			if (sum == s) {
+				answer++;
+			}
+
+		} else {
+
+			// 사용
+			pick[index] = true;
+			subset(pick, count + 1, index + 1);
+
+			// 사용하지 않음
+			pick[index] = false;
+			subset(pick, count, index + 1);
+
+		}
+	}
 }
