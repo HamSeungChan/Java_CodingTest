52005874d4c45a0da2a4daf8839845ed76afbbe9
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-05-04 12:36:52 +0900

    [Silver III] Title: N과 M (4), Time: 108 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,32 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n;
-    static int m;
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+	static int n, m;
+	static StringBuilder sb = new StringBuilder();
 
-        int [] array = new int[m];
-        new Main().DFS(array,0,1);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(br.readLine(), " ");
 
-    }
+		n = Integer.parseInt(token.nextToken());
+		m = Integer.parseInt(token.nextToken());
 
-    public void DFS(int[] array, int value, int start){
-        if(value == m){
-            for(int x: array){
-                System.out.print(x+" ");
-            }
-            System.out.println();
-        }
-        else{
-            for(int i= start; i<=n;i++){
-                array[value] = i;
-                DFS(array, value+1,i);
-            }
-        }
-    }
+		comb(0, 0, new int[m]);
+		System.out.print(sb);
+	}
+
+	public static void comb(int index, int start, int[] array) {
+		if (index == m) {
+			for (int i : array) {
+				sb.append(i + 1).append(" ");
+			}
+			sb.append("\n");
+		} else {
+			for (int i = start; i < n; i++) {
+				array[index] = i;
+				comb(index + 1, i, array);
+			}
+		}
+	}
 
 }
