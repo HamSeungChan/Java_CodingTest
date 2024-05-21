abe506e2d41f99c905c06970c96e98b337eff3db
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-05-21 12:58:25 +0900

    [Silver II] Title: 영재의 시험, Time: 120 ms, Memory: 11660 KB -BaekjoonHub

--- 백준/Silver/19949. 영재의 시험/영재의 시험.java
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.java
@@ -17,40 +17,23 @@ public class Main {
 		for (int i = 0; i < 10; i++) {
 			array[i] = Integer.parseInt(token.nextToken());
 		}
-		recursion(new int[10], 0, 0, 0);
+		recursion(0, 0, 0, 0);
 		System.out.println(answer);
 	}
 
-	public static void recursion(int[] pick, int index, int bb, int b) {
+	public static void recursion(int index, int bb, int b, int score) {
 
 		if (index == 10) {
-			int count = 0;
-			for (int i = 0; i < 10; i++) {
-				if (pick[i] == array[i]) {
-					count++;
-				}
-			}
-			if (count >= 5) {
+			if (score >= 5) {
 				answer++;
 			}
 			return;
 		}
 
 		for (int i = 1; i <= 5; i++) {
-
-			if (index == 0) {
-				pick[index] = i;
-				recursion(pick, index + 1, -1, i);
-			} else if (index == 1) {
-				pick[index] = i;
-				recursion(pick, index + 1, b, i);
-			} else {
-				if (bb == b && b == i) {
-					continue;
-				}
-				pick[index] = i;
-				recursion(pick, index + 1, b, i);
-			}
+			if (bb == b && b == i)
+				continue;
+			recursion(index + 1, b, i, score + (i == array[index] ? 1 : 0));
 		}
 
 	}
