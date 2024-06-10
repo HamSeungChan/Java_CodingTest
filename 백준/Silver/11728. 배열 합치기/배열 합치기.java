427feed804ed30f74e2c9e0f0df0c17a68821542
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-06-10 22:51:44 +0900

    [Silver V] Title: 배열 합치기, Time: 1432 ms, Memory: 308380 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,34 +1,54 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+    public static void main(String[] args) throws IOException {
 
-		String[] input=br.readLine().split(" "); 
-		int n=Integer.parseInt(input[0]), m=Integer.parseInt(input[1]);
-		int size=n+m;
-		int[] result=new int[size];
-		
-		String[] arrayA=br.readLine().split(" "); 
-		String[] arrayB=br.readLine().split(" "); 
-		
-		int start=0;
-		for(int i=0;i<n;i++) { 
-			result[start++]=Integer.parseInt(arrayA[i]);
-		}
-		for(int i=0;i<m;i++) { 
-			result[start++]=Integer.parseInt(arrayB[i]);
-		}
-		Arrays.sort(result); 
-		
-		for(int r:result) {
-			bw.write(String.valueOf(r)+" ");
-		}
-		
-		bw.flush();
-		bw.close();
-	}
-    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(token.nextToken());
+        int m = Integer.parseInt(token.nextToken());
+
+        int[] arrayA = new int[n];
+        int[] arrayB = new int[m];
+
+        token = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arrayA[i] = Integer.parseInt(token.nextToken());
+        }
+
+        token = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < m; i++) {
+            arrayB[i] = Integer.parseInt(token.nextToken());
+        }
+
+        int[] answer = new int[n + m];
+        int a = 0, b = 0;
+        for (int i = 0; i < answer.length; i++) {
+
+            if (a == n) {
+                answer[i] = arrayB[b];
+                b++;
+            } else if (b == m) {
+                answer[i] = arrayA[a];
+                a++;
+            } else {
+                if (arrayA[a] < arrayB[b]) {
+                    answer[i] = arrayA[a];
+                    a++;
+                } else {
+                    answer[i] = arrayB[b];
+                    b++;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i : answer) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+    }
 }
