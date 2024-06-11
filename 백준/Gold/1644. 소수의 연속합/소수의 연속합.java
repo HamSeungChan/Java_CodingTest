165d7601df59e0174403ce7a2f42d852cd420814
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-06-11 14:01:08 +0900

    [Gold III] Title: 소수의 연속합, Time: 212 ms, Memory: 27524 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,32 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int count = 0;
-        int[] array = new int[n + 1];
-        List<Integer> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        boolean[] array = new boolean[n + 1];
+        List<Integer> list = new ArrayList<>();
         for (int i = 2; i <= n; i++) {
-            if (array[i] == 0) {
+            if (!array[i]) {
                 list.add(i);
-                for (int j = i; j <= n; j = i + j){
-                    array[j] = 1;
+                for (int j = i * 2; j <= n; j += i) {
+                    array[j] = true;
                 }
             }
         }
-        int lt = 0, sum = 0;
-        for (int rt : list) {
-            sum += rt;
-            if (sum == n) count++;
-            while (sum >= n) {
-                sum -= list.get(lt++);
-                if (sum == n) count++;
+
+        int lt = 0;
+        int sum = 0;
+        int answer = 0;
+        for (int rt = 0; rt < list.size(); rt++) {
+            sum += list.get(rt);
+            while (sum > n) {
+                sum -= list.get(lt);
+                lt++;
+            }
+            if (sum == n) {
+                answer++;
             }
         }
-        System.out.println(count);
+        System.out.println(answer);
     }
 }
