6f19bc14d7bbffa9ff892c671eef32a5710aa94a
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-06-28 12:42:05 +0900

    [Silver I] Title: 단지번호붙이기, Time: 132 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,4 +1,72 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    static int[][] array;
+    static boolean[][] check;
+    static int n;
+    static int[] move_x = {1, 0, -1, 0};
+    static int[] move_y = {0, 1, 0, -1};
+    static int count = 0;
+    static int size = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        array = new int[n][n];
+        check = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            String[] tmp = br.readLine().split("");
+            for (int j = 0; j < n; j++) {
+                array[i][j] = Integer.parseInt(tmp[j]);
+            }
+        }
+
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!check[i][j] && array[i][j] != 0) {
+                    count++;
+                    size = 0;
+                    dfs(new Point(i, j));
+                    answer.add(size);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(count).append("\n");
+        Collections.sort(answer);
+        for (Integer i : answer) {
+            sb.append(i).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    public static void dfs(Point p) {
+
+        check[p.x][p.y] = true;
+        size++;
+
+        for (int i = 0; i < 4; i++) {
+            int moveX = p.x + move_x[i];
+            int moveY = p.y + move_y[i];
+            if (canMove(moveX, moveY)) {
+                dfs(new Point(moveX, moveY));
+            }
+        }
+    }
+
+    public static boolean canMove(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n && array[x][y] != 0 && !check[x][y];
+    }
+}
 
 class Point {
     int x;
@@ -7,65 +75,5 @@ class Point {
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-}
-
-public class Main {
-
-    public static final int[] MOVE_X = {0, 1, 0, -1};
-    public static final int[] MOVE_Y = {1, 0, -1, 0};
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Main m = new Main();
-        int n = sc.nextInt();
-        int[][] graph = new int[n][n];
-        int answer = 0;
-        List<Integer> houseCount = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            String s = sc.next();
-            for (int j = 0; j < n; j++) {
-                graph[i][j] = Character.getNumericValue(s.charAt(j));
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (graph[i][j] == 1) {
-                    answer++;
-                    houseCount.add(m.BFS(graph, new Point(i, j), n));
-                }
-            }
-        }
-        System.out.print(answer);
-        Collections.sort(houseCount);
-        for (int x : houseCount) {
-            System.out.println();
-            if (x != 0) {
-                System.out.print(x);
-            }
-        }
-    }
-
-    public int BFS(int[][] graph, Point p, int n) {
-        Queue<Point> q = new LinkedList<>();
-        q.offer(p);
-        int houseCount = 1;
-        graph[p.x][p.y] = 0;
-        while (!q.isEmpty()) {
-            Point tmp = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int mX = tmp.x + MOVE_X[i];
-                int mY = tmp.y + MOVE_Y[i];
-
-                if (0 <= mX && mX < n && 0 <= mY && mY < n && graph[mX][mY] == 1) {
-                    q.offer(new Point(mX, mY));
-                    graph[mX][mY] = 0;
-                    houseCount++;
-                }
-            }
-        }
-        return houseCount;
     }
 }
