d4c2f35f4713aaeb63b5facd398c9970434cd291
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-07-01 16:03:38 +0900

    [Gold IV] Title: 알파벳, Time: 840 ms, Memory: 15588 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,79 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static char[][] array;
-	static int r, c;
-	static int answer = 1;
-	public static final int[] MOVE_X = { 1, 0, -1, 0 };
-	public static final int[] MOVE_Y = { 0, 1, 0, -1 };
+    static int[][] array;
+    static int count, r, c;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] tmp = br.readLine().split(" ");
-		r = Integer.parseInt(tmp[0]);
-		c = Integer.parseInt(tmp[1]);
+    static int[] MOVE_X = {1, 0, -1, 0};
+    static int[] MOVE_Y = {0, 1, 0, -1};
 
-		array = new char[r][c];
+    public static void main(String[] args) throws IOException {
 
-		for (int i = 0; i < r; i++) {
-			String s = br.readLine();
-			for (int j = 0; j < c; j++) {
-				array[i][j] = s.charAt(j);
-			}
-		}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
 
-	
-		int size = 'Z'-'A';
-		boolean[] check = new boolean[size + 1];
-		
+        r = Integer.parseInt(token.nextToken());
+        c = Integer.parseInt(token.nextToken());
 
-		check[array[0][0] - 'A'] = true;
-		
-		DFS(new Point(0, 0), 1, check);
-		System.out.println(answer);
-		
+        array = new int[r][c];
 
-	}
+        for (int i = 0; i < r; i++) {
+            char[] tmp = br.readLine().toCharArray();
+            for (int j = 0; j < c; j++) {
+                array[i][j] = tmp[j] - 'A';
+            }
+        }
 
-	public static void DFS(Point p, int moveCount, boolean[] check) {
+        count = 0;
+        dfs(0, 0, new boolean[26], 1);
+        System.out.println(count);
+    }
 
-		for (int i = 0; i < MOVE_X.length; i++) {
-			int moveX = p.x + MOVE_X[i];
-			int moveY = p.y + MOVE_Y[i];
+    public static void dfs(int r, int c, boolean[] use, int moveCount) {
 
-			if (checkValid(moveX, moveY) && !checkDuplicate(array[moveX][moveY], check)) {
-				char c = array[moveX][moveY];
+        use[array[r][c]] = true;
+        count = Math.max(moveCount, count);
 
-				moveCount++;
-				answer = Math.max(answer, moveCount);
-				check[c - 'A'] = true;
-				DFS(new Point(moveX, moveY), moveCount, check);
-				check[c - 'A'] = false;
-				moveCount--;
-			}
-		}
-	}
+        for (int i = 0; i < 4; i++) {
+            int moveX = r + MOVE_X[i];
+            int moveY = c + MOVE_Y[i];
 
-	public static boolean checkValid(int x, int y) {
-		return x >= 0 && x < r && y >= 0 && y < c;
-	}
+            if (canMove(moveX, moveY, use)) {
+                dfs(moveX, moveY, use, moveCount + 1);
+            }
+        }
+        use[array[r][c]] = false;
+    }
 
-	public static boolean checkDuplicate(char c, boolean[] check) {
-		
-		return check[c - 'A'];
-	}
-
-}
-
-class Point {
-	int x;
-	int y;
-
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+    public static boolean canMove(int x, int y, boolean[] use) {
+        return x >= 0 && x < r && y >= 0 && y < c && !use[array[x][y]];
+    }
 }
