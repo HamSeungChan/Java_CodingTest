df0ddcdcc0dd65ce1a9062283c1e07f029eaf863
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-07-01 18:04:42 +0900

    [Gold II] Title: 빵집, Time: 376 ms, Memory: 39812 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -5,64 +5,58 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static char[][] array;
-	static boolean[][] pipeLine;
-	static int answer, r, c;
-	static boolean flag;
-	static final int[] MOVE_X = { -1, 0, 1 };
+    static int r, c, answer = 0;
+    static boolean flag;
+    static char[][] map;
+    static boolean[][] use;
+    static int[] MOVE_X = {-1, 0, 1};
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer token = new StringTokenizer(br.readLine(), " ");
-		r = Integer.parseInt(token.nextToken());
-		c = Integer.parseInt(token.nextToken());
+    public static void main(String[] args) throws IOException {
 
-		pipeLine = new boolean[r][c];
-		array = new char[r][c];
-		answer = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+        r = Integer.parseInt(token.nextToken());
+        c = Integer.parseInt(token.nextToken());
 
-		for (int i = 0; i < r; i++) {
-			String tmp = br.readLine();
-			for (int j = 0; j < c; j++) {
-				array[i][j] = tmp.charAt(j);
-			}
-		}
+        map = new char[r][c];
+        use = new boolean[r][c];
 
-		for (int i = 0; i < r; i++) {
-			flag = false;
-			setPipe(i, 0);
-		}
+        for (int i = 0; i < r; i++) {
+            String tmp = br.readLine();
+            for (int j = 0; j < c; j++) {
+                map[i][j] = tmp.charAt(j);
+            }
+        }
 
-		System.out.println(answer);
+        for (int i = 0; i < r; i++) {
+            flag = false;
+            setPipe(i, 0);
+        }
+        System.out.println(answer);
+    }
 
-	}
+    public static void setPipe(int x, int y) {
 
-	public static void setPipe(int x, int y) {
+        use[x][y] = true;
 
-		if (y == c - 1) {
-			answer++;
-			flag = true;
-			return;
-		}
+        if (y == c - 1) {
+            flag = true;
+            answer++;
+            return;
+        }
 
-		for (int i = 0; i < MOVE_X.length; i++) {
-			int moveX = x + MOVE_X[i];
-			int moveY = y + 1;
-			if (checkValid(moveX, moveY)) {
-				pipeLine[moveX][moveY] = true;
-				setPipe(moveX, moveY);
-				if (flag) {
-					break;
-				}
-			}
-		}
-	}
+        for (int i = 0; i < 3; i++) {
+            int moveX = x + MOVE_X[i];
+            if (canMove(moveX, y + 1)) {
+                setPipe(moveX, y + 1);
+                if (flag) {
+                    return;
+                }
+            }
+        }
+    }
 
-	public static boolean checkValid(int x, int y) {
-		if (x >= 0 && x < r && array[x][y] != 'x' && pipeLine[x][y] == false) {
-			return true;
-		}
-		return false;
-	}
-
+    public static boolean canMove(int x, int y) {
+        return 0 <= x && x < r && !use[x][y] && map[x][y] != 'x';
+    }
 }
