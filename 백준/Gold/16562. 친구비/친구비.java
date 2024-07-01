6a9b51623e33fbd026f131a9931cec323194f931
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-07-01 15:28:06 +0900

    [Gold IV] Title: 친구비, Time: 220 ms, Memory: 20948 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.java
+++ 백준/Gold/16562. 친구비/친구비.java
@@ -1,82 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] parents;
-    static int[] cost;
-
-    static int find(int value) {
-
-        if (value == parents[value]) {
-            return value;
-        }
-
-        return parents[value] = find(parents[value]);
-    }
-
-    static void union(int a, int b) {
-        int findA = find(a);
-        int findB = find(b);
-
-        if (findA != findB) {
-            if (cost[findA] < cost[findB]) {
-                parents[findB] = findA;
-            } else {
-                parents[findA] = findB;
-            }
-        }
-    }
+    static List<List<Integer>> graph = new ArrayList<>();
+    static int[] array;
+    static boolean[] check;
+    static int minValue;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(token.nextToken());
-        int m = Integer.parseInt(token.nextToken());
-        int k = Integer.parseInt(token.nextToken());
 
+        int n = Integer.parseInt(token.nextToken());    // 학생 수
+        int m = Integer.parseInt(token.nextToken());    // 친구관계 수
+        int k = Integer.parseInt(token.nextToken());    // 가지고 있는 돈
 
-        parents = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            parents[i] = i;
-        }
-
-        // 학생들이 요구하는 친구비
+        array = new int[n + 1];  // 친구비를 저장하는 배열
+        check = new boolean[n + 1]; // dfs 방문 여부를 확인하는 배열
         token = new StringTokenizer(br.readLine(), " ");
-        cost = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            cost[i] = Integer.parseInt(token.nextToken());
+            array[i] = Integer.parseInt(token.nextToken());
         }
 
+        // 친구들간의 연관관계
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
         for (int i = 0; i < m; i++) {
-            token = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(token.nextToken());
-            int b = Integer.parseInt(token.nextToken());
-            union(a, b);
+            token = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(token.nextToken());
+            int to = Integer.parseInt(token.nextToken());
+            graph.get(from).add(to);
+            graph.get(to).add(from);
         }
 
-        Set<Integer> set = new HashSet<>();
+        int sum = 0;
         for (int i = 1; i <= n; i++) {
-            set.add(find(i));
+            if (!check[i]) {
+                minValue = array[i];
+                dfs(i);
+                sum += minValue;
+            }
         }
+        System.out.println(sum <= k ? sum : "Oh no");
+    }
 
-        Iterator<Integer> iterator = set.iterator();
-        long sum = 0;
-        while (iterator.hasNext()) {
-            Integer next = iterator.next();
-//            System.out.println(next);
-            sum += cost[next];
-        }
+    public static void dfs(int now) {
 
-        if (sum > k) {
-            System.out.println("Oh no");
-        } else {
-            System.out.println(sum);
+        check[now] = true;
+        minValue = Math.min(minValue, array[now]);
+
+        List<Integer> list = graph.get(now);
+        for (Integer next : list) {
+            if (!check[next]) {
+                dfs(next);
+            }
         }
     }
 }
