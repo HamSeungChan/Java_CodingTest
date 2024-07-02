2747d42f5a8531b6a51eacf38f44f5aacbf7d65f
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-07-02 15:43:13 +0900

    [Gold IV] Title: 열 정렬정렬 정, Time: 544 ms, Memory: 37872 KB -BaekjoonHub

--- 백준/Gold/25577. 열 정렬정렬 정/열 정렬정렬 정.java
+++ 백준/Gold/25577. 열 정렬정렬 정/열 정렬정렬 정.java
@@ -1,70 +1,48 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] array, sortArray, check;
-    static int size = 0;
-
+    static int[][] array;
+    static boolean[] check;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        array = new int[n];
-        sortArray = new int[n];
-        check = new int[n];
+        array = new int[n][2];
+        check = new boolean[n];
 
-        Arrays.fill(check, -1);
         StringTokenizer token = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            int tmp = Integer.parseInt(token.nextToken());
-            array[i] = tmp;
-            sortArray[i] = tmp;
+            array[i][0] = Integer.parseInt(token.nextToken());
+            array[i][1] = i;
         }
-        Arrays.sort(sortArray);
-
-        Map<Integer, Integer> map = new HashMap<>();
-
-        int value = 0;
-        for (int i = 0; i < n; i++) {
-            int tmp = sortArray[i];
-            map.put(tmp, value++);
-        }
-
-        for (int i = 0; i < n; i++) {
-            array[i] = map.get(array[i]);
-            sortArray[i] = array[i];
-        }
-        Arrays.sort(sortArray);
+        Arrays.sort(array, Comparator.comparingInt(o -> o[0]));
 
         int answer = 0;
         for (int i = 0; i < n; i++) {
-            size = 0;
-            if (check[sortArray[i]] == -1) {
-                dfs(sortArray[i]);
-            }
-            if (size > 0) {
-                answer += (size - 1);
+            if (!check[i]) {
+                answer += dfs(i);
             }
         }
         System.out.println(answer);
     }
 
-    public static void dfs(int now) {
+    public static int dfs(int now) {
 
-        int next = array[now];
-        if (check[next] == -1) {
-            check[next] = check[now] + 1;
-            dfs(sortArray[next]);
-        } else if (next != now) {
-            size = check[now] - check[next] + 1;
+        check[now] = true;
+        int size = 0;
+        if (!check[array[now][1]]) {
+            size = dfs(array[now][1]) + 1;
         }
+
+        return size;
     }
+
 }
