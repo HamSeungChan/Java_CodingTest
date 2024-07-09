93a9ffe06a93f0b1d9a8adcb9539d259e006851f
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-07-09 14:57:14 +0900

    [Gold IV] Title: 알고스팟, Time: 312 ms, Memory: 36472 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -8,66 +8,67 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] array;
+    static int[][] map, check;
     static int n, m;
     static int[] MOVE_X = {1, 0, -1, 0};
     static int[] MOVE_Y = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+
         m = Integer.parseInt(token.nextToken());
         n = Integer.parseInt(token.nextToken());
-        array = new int[n + 1][m + 1];
-        for (int i = 1; i <= n; i++) {
-            String[] s = br.readLine().split("");
-            for (int j = 1; j <= m; j++) {
-                array[i][j] = Integer.parseInt(s[j - 1]);
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String[] tmp = br.readLine().split("");
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(tmp[j]);
             }
         }
 
-        int[][] value = new int[n + 1][m + 1];
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(value[i], 100_000_000);
+        check = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(check[i], Integer.MAX_VALUE);
         }
 
-        if (n == 1 && m == 1) {
-            System.out.println(0);
-        } else {
-            bfs(value);
-            System.out.println(value[n][m]);
-        }
-
-    }
-
-    public static void bfs(int[][] value) {
         Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(1, 1));
-        value[1][1] = 0;
+        q.offer(new Point(0, 0, 0));
+        check[0][0] = 0;
+
         while (!q.isEmpty()) {
             Point tmp = q.poll();
-            for (int i = 0; i < MOVE_X.length; i++) {
+            for (int i = 0; i < 4; i++) {
                 int moveX = tmp.x + MOVE_X[i];
                 int moveY = tmp.y + MOVE_Y[i];
-                if (range(moveX, moveY) && value[moveX][moveY] > value[tmp.x][tmp.y] + array[moveX][moveY]) {
-                    q.offer(new Point(moveX, moveY));
-                    value[moveX][moveY] = value[tmp.x][tmp.y] + array[moveX][moveY];
+                if (canMove(moveX, moveY)) {
+                    int newValue = tmp.count + map[moveX][moveY];
+                    if (check[moveX][moveY] > newValue) {
+                        check[moveX][moveY] = newValue;
+                        q.offer(new Point(moveX, moveY, newValue));
+                    }
                 }
             }
         }
+
+        System.out.println(check[n - 1][m - 1]);
     }
 
-    public static boolean range(int x, int y) {
-        return x >= 1 && x <= n && y >= 1 && y <= m;
+
+    public static boolean canMove(int moveX, int moveY) {
+        return 0 <= moveX && moveX < n && 0 <= moveY && moveY < m;
     }
 }
 
 class Point {
     int x;
     int y;
+    int count;
 
-    public Point(int x, int y) {
+    public Point(int x, int y, int count) {
         this.x = x;
         this.y = y;
+        this.count = count;
     }
 }
