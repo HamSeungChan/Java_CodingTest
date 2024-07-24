878484c8d95f68e3f19eff5832c04adf4799ae7d
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-07-24 21:03:13 +0900

    [Silver II] Title: 창고 다각형, Time: 148 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -4,55 +4,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[1001];
 
-		int n = Integer.parseInt(br.readLine());
+        int maxHigh = 0;
+        StringTokenizer token;
+        for (int i = 0; i < n; i++) {
+            token = new StringTokenizer(br.readLine(), " ");
+            int l = Integer.parseInt(token.nextToken());
+            int h = Integer.parseInt(token.nextToken());
+            maxHigh = Math.max(maxHigh, h);
+            array[l] = h;
+        }
 
-		int maxHigh = Integer.MIN_VALUE;
-		int maxRight = Integer.MIN_VALUE;
+        int answer = 0;
+        for (int i = 1; i <= maxHigh; i++) {
 
-		int[] array = new int[1001];
-
-		StringTokenizer token;
-		for (int i = 0; i < n; i++) {
-			token = new StringTokenizer(br.readLine(), " ");
-
-			int l = Integer.parseInt(token.nextToken());
-			int h = Integer.parseInt(token.nextToken());
-
-			maxRight = Math.max(maxRight, l);
-			maxHigh = Math.max(maxHigh, h);
-
-			array[l] = h;
-		}
-
-		int count = 0;
-
-		for (int i = 1; i <= maxHigh; i++) {
-
-			int startPoint = 0;
-			int endPoint = 0;
-
-			// 좌측부터 시작 포인트
-			for (int j = 0; j <= maxRight; j++) {
-				if (array[j] >= i) {
-					startPoint = j;
-					break;
-				}
-			}
-
-			// 우측부터 끝나는 포인트
-			for (int j = maxRight; j >= 0; j--) {
-				if (array[j] >= i) {
-					endPoint = j;
-					break;
-				}
-			}
-
-			count += endPoint - startPoint + 1;
-		}
-		System.out.println(count);
-	}
+            int startPoint = 0;
+            int endPoint = 0;
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] >= i) {
+                    if (startPoint == 0) {
+                        startPoint = j;
+                    }
+                    endPoint = j;
+                }
+            }
+            answer += (endPoint - startPoint + 1);
+        }
+        System.out.println(answer);
+    }
 }
