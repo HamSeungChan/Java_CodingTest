3c0ee921680fab7745c5e93c0cb03a2bd8317094
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-08-22 13:37:56 +0900

    [Silver II] Title: 창고 다각형, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,39 +1,48 @@
+import com.sun.security.jgss.GSSUtil;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] array = new int[1001];
 
-        int maxHigh = 0;
+        int[] array = new int[1002];
+        int start = Integer.MAX_VALUE;
+        int end = Integer.MIN_VALUE;
+
         StringTokenizer token;
         for (int i = 0; i < n; i++) {
             token = new StringTokenizer(br.readLine(), " ");
-            int l = Integer.parseInt(token.nextToken());
-            int h = Integer.parseInt(token.nextToken());
-            maxHigh = Math.max(maxHigh, h);
-            array[l] = h;
+            int position = Integer.parseInt(token.nextToken());
+            int height = Integer.parseInt(token.nextToken());
+            array[position] = height;
+
+            start = Math.min(start, position);
+            end = Math.max(end, position);
+        }
+
+        int[] leftMax = new int[1002];
+        int[] rightMax = new int[1002];
+
+        for (int i = start; i <= end; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], array[i]);
+        }
+
+        for (int i = end; i >= start; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], array[i]);
         }
 
         int answer = 0;
-        for (int i = 1; i <= maxHigh; i++) {
-
-            int startPoint = 0;
-            int endPoint = 0;
-            for (int j = 0; j < array.length; j++) {
-                if (array[j] >= i) {
-                    if (startPoint == 0) {
-                        startPoint = j;
-                    }
-                    endPoint = j;
-                }
-            }
-            answer += (endPoint - startPoint + 1);
+        for (int i = start; i <= end; i++) {
+            answer += Math.min(leftMax[i], rightMax[i]);
         }
         System.out.println(answer);
     }
