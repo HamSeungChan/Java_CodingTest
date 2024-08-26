c2b1ce2f96cf072ec146c658ef0e1b11d4a3b7dd
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-08-26 21:43:37 +0900

    [Gold IV] Title: 점수따먹기, Time: 680 ms, Memory: 20284 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -4,35 +4,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer token = new StringTokenizer(br.readLine(), " ");
-
         int n = Integer.parseInt(token.nextToken());
         int m = Integer.parseInt(token.nextToken());
-
-        int[][] dp = new int[n + 1][m + 1];
-
+        int[][] array = new int[n + 1][m + 1];
         for (int i = 1; i <= n; i++) {
             token = new StringTokenizer(br.readLine(), " ");
             for (int j = 1; j <= m; j++) {
-                int value = Integer.parseInt(token.nextToken());
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + value;
+                array[i][j] = array[i - 1][j] + array[i][j - 1] - array[i - 1][j - 1] + Integer.parseInt(token.nextToken());
             }
         }
-
         int answer = Integer.MIN_VALUE;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                for (int k = i; k <= n; k++) {
-                    for (int l = j; l <= m; l++) {
-                        answer = Math.max(answer, dp[k][l] - dp[k - i][l] - dp[k][l - j] + dp[k - i][l - j]);
+                for (int l = 1; l <= i; l++) {
+                    for (int k = 1; k <= j; k++) {
+                        answer = Math.max(answer, array[i][j] - array[i - l][j] - array[i][j - k] + array[i - l][j - k]);
                     }
                 }
             }
         }
-
         System.out.println(answer);
     }
 }
