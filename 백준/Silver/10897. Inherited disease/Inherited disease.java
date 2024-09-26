a53438175f4876977d5addf2c2236be6c3e3306a
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-09-26 15:15:40 +0900

    [Silver I] Title: Inherited disease, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/10897. Inherited disease/Inherited disease.java
+++ 백준/Silver/10897. Inherited disease/Inherited disease.java
@@ -1,30 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
+
     static final int mod = 1000000007;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int D = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
 
         long numberCount = 0;
         long currentNumber = 1;
         long node = 1;
+        StringBuilder sb = new StringBuilder();
 
-        for(int i=1;i<=D;i++){
-            int d = Integer.parseInt(st.nextToken());
-            currentNumber = ((currentNumber-1)*i+d)%mod;
-            System.out.println((numberCount+currentNumber)%mod);
-            node = (node*i)%mod;
-            numberCount = (numberCount+node)%mod;
+        for (int i = 1; i <= D; i++) {
+
+            int d = Integer.parseInt(token.nextToken());
+            currentNumber = ((currentNumber - 1) * i + d) % mod;
+
+            sb.append((numberCount + currentNumber) % mod).append("\n");
+            node = (node * i) % mod;
+            numberCount = (numberCount + node) % mod;
+
         }
+        System.out.println(sb);
     }
 }
