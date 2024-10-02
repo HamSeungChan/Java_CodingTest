56f865fec61310d9e37ea3a5ac58a83868b7797d
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2024-10-02 20:54:02 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14704 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,21 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
+	static int n, answer = Integer.MAX_VALUE;
 	static int[][] array;
 	static int[][] dp;
-	static int n, answer = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		n = Integer.parseInt(br.readLine());
 		array = new int[n][4];
-		dp = new int[n][4];
+		dp = new int[n + 1][4];
+
+		for (int i = 0; i < dp.length; i++) {
+			Arrays.fill(dp[i], -1);
+		}
 
 		StringTokenizer token;
 		for (int i = 0; i < n; i++) {
@@ -24,39 +28,27 @@ public class Main {
 				array[i][j] = Integer.parseInt(token.nextToken());
 			}
 		}
-
 		System.out.println(recursion(0, 0));
-
 	}
 
 	public static int recursion(int index, int before) {
 
-		// SUM 에 값을 누적하고 있다.
-		// RETURN 하려는 값은 SUM 이다.
-		// SUM이랑 ANSWER를 뺀다
 		if (index == n) {
 			return 0;
 		}
 
-		if (dp[index][before] != 0) {
+		if (dp[index][before] != -1) {
 			return dp[index][before];
 		}
 
-		int ret = 1000000000;
+		int tmp = Integer.MAX_VALUE;
 
 		for (int i = 1; i <= 3; i++) {
-			if (before == i)
-				continue;
-			ret = Math.min(ret, recursion(index + 1, i) + array[index][i]);
+			if (before != i) {
+				tmp = Math.min(tmp, recursion(index + 1, i) + array[index][i]);
+			}
 		}
-		return dp[index][before] = ret;
+		dp[index][before] = tmp;
+		return tmp;
 	}
 }
-
-
-/*
- * 탑다운 dp 짜는 법
- * 1. 백트래킹을 짠다 / VOID - > RETURN 타입으로
- * 2. 리턴하는 방식을 바꾼다
- * 3. 메모이제이션 -> 인자가 2개 임으로 DP 테이블도 2개로 설정
- * */
