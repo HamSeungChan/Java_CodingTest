dcca6e5a1f8869b09e1c124d7795d541b402a9d1
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2025-03-05 20:59:20 +0900

    [Silver I] Title: 정수 삼각형, Time: 316 ms, Memory: 30264 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,22 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int [][] tmp = new int[n][n];
 
-        for(int i = 0; i< n;i++){
-            for(int j = 0; j<= i;j++){
-                tmp[i][j] = sc.nextInt();
+    static int n;
+    static List<List<Integer>> map;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n + 2][n + 1];
+        for (int i = 0; i <= n + 1; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        map = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            map.add(new ArrayList<>());
+        }
+
+        StringTokenizer token;
+        for (int i = 1; i <= n; i++) {
+            List<Integer> now = map.get(i);
+            token = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < i; j++) {
+                now.add(Integer.parseInt(token.nextToken()));
             }
         }
 
-        for(int i = n-2; i>=0 ; i--){
-            for(int j=0;j<=i;j++){
-                tmp[i][j] += Math.max(tmp[i+1][j],tmp[i+1][j+1]);
-            }
+        System.out.println(recursion(2, 0) + map.get(1).get(0));
+    }
+
+    public static int recursion(int height, int beforeIndex) {
+
+        if (dp[height][beforeIndex] != -1) {
+            return dp[height][beforeIndex];
         }
-        System.out.println(tmp[0][0]);
+
+        if (height == n + 1) {
+            return 0;
+        }
+
+        List<Integer> now = map.get(height);
+        return dp[height][beforeIndex] =
+                Math.max(recursion(height + 1, beforeIndex) + now.get(beforeIndex),
+                        recursion(height + 1, beforeIndex + 1) + now.get(beforeIndex + 1));
     }
 }
