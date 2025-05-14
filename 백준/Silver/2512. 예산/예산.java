5b1cf55d7ad62f7fb77b9f9799fd01888e0b5856
Hamburgerkin9 <66303405+HamSeungChan@users.noreply.github.com>
2025-05-14 21:20:27 +0900

    [Silver II] Title: 예산, Time: 148 ms, Memory: 15220 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,55 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int[] array;
-
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
 
-        array = new int[n];
         StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+        int max = 0;
         for (int i = 0; i < n; i++) {
             array[i] = Integer.parseInt(token.nextToken());
+            max = Math.max(max, array[i]);
         }
 
         int m = Integer.parseInt(br.readLine());
 
-        Arrays.sort(array);
-
         int lt = 0;
-        int rt = array[n - 1];
+        int rt = max;
+        int answer = -1;
 
-        int answer = 0;
         while (lt <= rt) {
-            int middle = (lt + rt) / 2;
-            int sum = sum(middle);
-
-            if (sum > m) {
-                rt = middle - 1;
-            } else {
-                answer = Math.max(middle, answer);
-                lt = middle + 1;
+            int mid = (lt + rt) / 2;
+            if (solution(mid, m, array)) {
+                answer = Math.max(answer, mid);
+                lt = mid + 1;
+                continue;
             }
+            rt = mid - 1;
         }
+
         System.out.println(answer);
     }
 
-    public static int sum(int middle) {
+    public static boolean solution(int value, int m, int[] array) {
 
-        int sum = 0;
+        int total = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > middle) {
-                sum += middle;
+            if (array[i] <= value) {
+                total += array[i];
             } else {
-                sum += array[i];
+                total += value;
+            }
+
+            if (total > m) {
+                return false;
             }
         }
-        return sum;
+        return true;
     }
 }
